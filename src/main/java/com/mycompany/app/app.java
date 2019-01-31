6598d2dc6ee5dca48132ed52c0202ccdb0565c0b
sidharth04 <42831275+sidharth04@users.noreply.github.com>
2019-01-31 18:51:42 +0530

    3

--- src/main/java/com/mycompany/app/app.java
+++ src/main/java/com/mycompany/app/app.java
@@ -1,5 +1,17 @@
-public class app{
-public static void main(String args[]){
-System.out.println("hello javatpoint");
-}}
+package com.mycompany.app;
+public class App
+{
 
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
