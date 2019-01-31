ce7dba1737d5f87c43f73d659d30d145a723eec9
sidharth04 <42831275+sidharth04@users.noreply.github.com>
2019-01-31 18:42:44 +0530

    first_update_code

--- src/main/java/com/mycompany/app/app.java
+++ src/main/java/com/mycompany/app/app.java
@@ -1,21 +1,5 @@
-package com.mycompany.app;
+public class Simple{
+public static void main(String args[]){
+System.out.println("hello javatpoint");
+}}
 
-/**
- * Hello world!
- */
-public class App
-{
-
-    private final String message = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
-
-    private final String getMessage() {
-        return message;
-    }
-
-}
